712eb1b49b394ed659612e4b45ed7989aeee1ada
palmandaniel <palman.daniel47@gmail.com>
2020-09-21 13:04:23 +0200

    mukodo auto osztaly

---
+++ JarmuPark/Auto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JarmuPark
+{
+    class Auto
+    {
+        private string szin;
+        private string tipus;
+        private double uzemAnyag;
+        private double fogyaszt;
+        private double km;
+        private double megtehetoKm;
+
+        public Auto(string szin, string tipus, double fogyaszt)
+        {
+            this.szin = szin;
+            this.tipus = tipus;
+            this.fogyaszt = fogyaszt;
+            this.uzemAnyag = 0;
+            this.km = 0;
+        }
+
+        private void MegtehetoKm()
+        {
+            megtehetoKm = uzemAnyag / fogyaszt * 100;
+        }
+
+        public void Tankol(double mennyit)
+        {
+            uzemAnyag += mennyit;
+            MegtehetoKm();
+        }
+
+        public void Megy(double km)
+        {
+            double fogy = fogyaszt / 100 * km;
+            this.km += km;
+
+            this.uzemAnyag = uzemAnyag - fogy;
+            MegtehetoKm();
+        }
+        public void Allapot()
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine($"{tipus} - {szin}");
+            Console.WriteLine($"Km óra: {km}");
+            Console.WriteLine($"Üzemanyag: {uzemAnyag} l");
+            Console.WriteLine($"Megtehető km {megtehetoKm} km");
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
--- JarmuPark/Program.cs
+++ JarmuPark/Program.cs
@@ -10,6 +10,26 @@ namespace JarmuPark
     {
         static void Main(string[] args)
         {
+            Auto lada = new Auto("piros", "Lada2112", 11);
+            Auto trabant = new Auto("zöld", "Trabant 1.1", 8);
+
+            lada.Tankol(20);
+            trabant.Tankol(20);
+
+            lada.Allapot();
+            trabant.Allapot();
+
+            Console.Write("Mennyit megy a Lada? (km) ");
+            double km1 = double.Parse(Console.ReadLine());
+            lada.Megy(km1);
+            lada.Allapot();
+
+            Console.Write("Mennyit megy a Trabant? (km) ");
+            double km2 = double.Parse(Console.ReadLine());
+            trabant.Megy(km2);
+            trabant.Allapot();
+
+            Console.ReadKey();
         }
     }
 }
